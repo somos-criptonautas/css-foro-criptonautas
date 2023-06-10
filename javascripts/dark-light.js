2dd673aa8ad8cdd2a26e3de5754aa4d2c8a10fc7
satoshinotdead <dev@criptonautas.co>
2023-06-10 03:57:10 -0300

    css-dark-test

--- javascripts/dark-light.js
+++ javascripts/dark-light.js
@@ -1,18 +1,26 @@
-  window.addEventListener("load", (event)=> {
-window.parent.postMessage("","*");
+  // Add an event listener for when the window loads
+window.addEventListener("load", (event) => {
+  // Post a message to the parent window
+  window.parent.postMessage("", "*");
 }, false);
 
-window.addEventListener("message", (event)=>{
-var origin = event.origin;
-if(origin === "https://criptonautas.co"){
-switch(event.data){
-case "light":
-document.body.classList = ['light'];
-break;
-case "dark":
-document.body.classList = ['dark'];
-break;
-}
-}
-},
-false);
+// Add an event listener for receiving messages
+window.addEventListener("message", (event) => {
+  // Get the origin of the message
+  var origin = event.origin;
+
+  // Check if the origin is the expected one
+  if (origin === "https://criptonautas.co") {
+    // Handle the message based on its data
+    switch (event.data) {
+      case "light":
+        // Set the body class to 'light'
+        document.body.classList.replace('dark', 'light');
+        break;
+      case "dark":
+        // Set the body class to 'dark'
+        document.body.classList.replace('light', 'dark');
+        break;
+    }
+  }
+}, false);
